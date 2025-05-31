85a28938ce959181ebe81390064f6495f108f636
Rohit Mali <rohitmaliarya@gmail.com>
2025-05-31 09:55:56 +0530

    31may

---
+++ insertionsort.java
@@ -0,0 +1,34 @@
+
+import java.util.Collection;
+
+class insertionsort{
+
+    public static void printarr(Integer arr[]){
+        for(Integer i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+            
+        }
+        System.out.println();
+    }
+        
+        public static void insertionsort( int arr[]){
+            for (int i=1;i<arr.length;i++){
+                int curr = arr[i];
+                int prev = i-1;
+                while(prev >=0 && arr[prev] > curr){
+                    arr[prev+1]=arr[prev];
+                    prev--;
+                }
+                
+                arr[prev+1] = curr;
+            }
+            
+        }
+    
+	public static void main(String[] args) {
+		Integer arr[]={2,5,1,4,6,3};
+		//insertionsort(arr);
+        Arrays.sort(arr,Collections.reverseOrder());
+		printarr(arr);
+	}
+}
